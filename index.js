c6b82713685b84142c0f944c4a607888823618be
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2020-10-19 09:16:21 -0600

    removed syntax error, now displays correctly

--- index.js
+++ index.js
@@ -67,5 +67,5 @@ app.use((err, req, res, next) => {
 
 // listen for requests
 app.listen(8080, () =>
-  console.log('Your app is listening on port 8080.');
+  console.log('Your app is listening on port 8080.')
 );
