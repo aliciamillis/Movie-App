3d573f0c163852b485c3a21dc0a5e69f60fa269d
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2021-02-04 11:06:36 -0700

    removed cors for now

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-const cors = require('cors');
+// const cors = require('cors');
 
 let allowedOrigins = *;
 
 const { check, validationResult } = require('express-validator');
 
-app.use(cors({
-      origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-          let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-          return callback(new Error(message), false);
-        }
-        return callback(null, true);
-      }
-    }));
+// app.use(cors({
+//       origin: (origin, callback) => {
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//           let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//           return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//       }
+//     }));
 
 
 // GET requests
